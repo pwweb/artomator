a13aa22ac94ce9bc121f994c7fbe4c47a406b0b9
Richard Browne <richard@my-sauce.co.uk>
2019-06-08 11:33:01 +0100

    Fix format of array definition

--- src/Commands/ArtomatorControllerCommand.php
+++ src/Commands/ArtomatorControllerCommand.php
@@ -89,7 +89,9 @@ class ArtomatorControllerCommand extends GeneratorCommand
         $replace["use {$controllerNamespace}\Controller;\n"] = '';
 
         return str_replace(
-            array_keys($replace), array_values($replace), parent::buildClass($name)
+            array_keys($replace),
+            array_values($replace),
+            parent::buildClass($name)
         );
     }
 
