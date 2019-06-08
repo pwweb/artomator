0e3ad002bf2766f3a590e4286240a841e25220b2
Richard Browne <richard@my-sauce.co.uk>
2019-06-08 15:59:06 +0100

    Make Controller use new DocBlock format

--- src/Commands/ArtomatorControllerCommand.php
+++ src/Commands/ArtomatorControllerCommand.php
@@ -44,7 +44,7 @@ class ArtomatorControllerCommand extends GeneratorCommand
      */
     protected function getStub()
     {
-        $stub = '/stubs/controller.model.stub';
+        $stub = 'controller.model.stub';
         $path = base_path() . config('artomator.stubPath');
         $path = $path . $stub;
 
@@ -140,7 +140,45 @@ class ArtomatorControllerCommand extends GeneratorCommand
             'DummyModelClass' => class_basename($modelClass),
             'DummyModelVariable' => lcfirst(class_basename($modelClass)),
             'DummyPackageVariable' => strtolower($this->package) . ".",
+            'DummyPackagePlaceholder' => config('app.name'),
+            'DummyCopyrightPlaceholder' => config('artomator.copyright'),
+            'DummyLicensePlaceholder' => config('artomator.license'),
+            'DummyAuthorPlaceholder' => $this->parseAuthors(config('artomator.authors')),
         ]);
+    }
+
+    /**
+     * Get the formatted author(s) from the config file.
+     *
+     * @param  array[string] $authors Authors array.
+     *
+     * @return string Formmated string of authors.
+     */
+    protected function parseAuthors($authors)
+    {
+        if (is_array($authors) === false and is_string($authors) === false) {
+            throw new InvalidArgumentException('Authors must be an array of strings or a string.');
+        }
+
+        $formatted = '';
+
+        if (is_array($authors) === true) {
+            if (is_string($authors[0]) === false) {
+                throw new InvalidArgumentException('The array of authors must be strings.');
+            }
+            $formatted .= array_shift($authors);
+
+            foreach ($authors as $author) {
+                if (is_string($author) === false) {
+                    throw new InvalidArgumentException('The array of authors must be strings.');
+                }
+                $formatted .= "\n * @author    " . $author;
+            }
+        } else {
+            $formatted .= $authors;
+        }
+
+        return $formatted;
     }
 
     /**
