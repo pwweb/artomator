79e1556d000d19b6428ab1638c526e9fd5da3039
Richard Browne <richard@my-sauce.co.uk>
2019-06-08 15:57:59 +0100

    Add attributes to config file for file docblock

--- config/artomator.php
+++ config/artomator.php
@@ -4,4 +4,13 @@ return [
     // The location of your custom stub files:
     'stubPath' => '/public/vendor/pwweb/',
 
+    'authors' => [
+        'Jane Doe <jane.doe@example.com>',
+        'Joe Blogs <joe.blogs@example.com>',
+    ],
+
+    'copyright' => '2019 ACME Corp.',
+
+    'license' => 'license.md',
+
 ];
