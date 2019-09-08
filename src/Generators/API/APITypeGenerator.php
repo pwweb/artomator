c515b2ccdfa9d5f0ced36bcd54be43916a16af50
Richard Browne <richard@my-sauce.co.uk>
2019-09-08 20:57:33 +0100

    Complete type generator

--- src/Generators/API/APITypeGenerator.php
+++ src/Generators/API/APITypeGenerator.php
@@ -27,19 +27,39 @@ class APITypeGenerator extends BaseGenerator
     {
         $this->commandData = $commandData;
         $this->path = $commandData->config->pathApiType;
-        $this->fileName = $this->commandData->modelName.'ApiType.php';
+        $this->fileName = $this->commandData->modelName.'Type.php';
     }
 
     public function generate()
     {
         $templateData = get_template('api.type.api_type', 'artomator');
+        $templateData = str_replace('$SCHEMA$', $this->generateSchema(), $templateData);
         $templateData = fill_template($this->commandData->dynamicVars, $templateData);
         $templateData = $this->fillDocs($templateData);
 
         FileUtil::createFile($this->path, $this->fileName, $templateData);
 
-        $this->commandData->commandObj->comment("\nApiType created: ");
+        $this->commandData->commandObj->comment("\nApi Type created: ");
         $this->commandData->commandObj->info($this->fileName);
+    }
+
+    private function generateSchema()
+    {
+        $schema = [];
+        foreach ($this->commandData->fields as $field) {
+            if (in_array($field->name, ['created_at','updated_at','id']) === true) {
+                continue;
+            }
+            if ($field->isNotNull === true) {
+                $field_type = "Type::nonNull(Type::" . $field->fieldType . "())";
+            } else {
+                $field_type = "Type::" . $field->fieldType . "()";
+            }
+
+            $schema[] = "'" . $field->name . "' => [" . arty_nl_tab(1, 4) . "'type' => " . $field_type . "," . arty_nl_tab(1, 4) . "'description' => 'The " . $field->name . " of the model'" . arty_nl_tab(1, 3) . "],";
+        }
+
+        return implode(arty_nl_tab(1, 3), $schema);
     }
 
     private function fillDocs($templateData)
