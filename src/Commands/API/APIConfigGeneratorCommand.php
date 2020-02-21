df31f36860448951060863c555e63a6f31415496
Richard Browne <richard@my-sauce.co.uk>
2020-02-21 14:53:34 +0000

    Add graphql config file generator

---
+++ src/Commands/API/APIConfigGeneratorCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace PWWEB\Artomator\Commands\API;
+
+use PWWEB\Artomator\Commands\BaseCommand;
+use PWWEB\Artomator\Common\CommandData;
+use PWWEB\Artomator\Generators\API\APIConfigGenerator;
+
+class APIConfigGeneratorCommand extends BaseCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'artomator.api:config';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create config command';
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->commandData = new CommandData($this, CommandData::$COMMAND_TYPE_API);
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        parent::handle();
+
+        $apiTypeGenerator = new APIConfigGenerator($this->commandData);
+        $apiTypeGenerator->generate();
+
+        $this->performPostActions();
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    public function getOptions()
+    {
+        return array_merge(parent::getOptions(), []);
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return array_merge(parent::getArguments(), []);
+    }
+}
