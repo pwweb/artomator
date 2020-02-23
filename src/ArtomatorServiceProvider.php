eca099239651d1a85e8e4caf5098434defa0d7fa
Richard Browne <richard@my-sauce.co.uk>
2020-02-23 13:46:09 +0000

    Refactored code to extend the base package instead of reinvent the wheel

--- src/ArtomatorServiceProvider.php
+++ src/ArtomatorServiceProvider.php
@@ -2,26 +2,12 @@
 
 namespace PWWEB\Artomator;
 
-use Illuminate\Support\ServiceProvider;
-use PWWEB\Artomator\Commands\API\APIQueryGeneratorCommand;
-use PWWEB\Artomator\Commands\API\APIGeneratorCommand;
-use PWWEB\Artomator\Commands\API\APIMutationsGeneratorCommand;
-use PWWEB\Artomator\Commands\API\APITypeGeneratorCommand;
-use PWWEB\Artomator\Commands\API\APIConfigGeneratorCommand;
-use PWWEB\Artomator\Commands\APIScaffoldGeneratorCommand;
-use PWWEB\Artomator\Commands\Common\MigrationGeneratorCommand;
-use PWWEB\Artomator\Commands\Common\ModelGeneratorCommand;
-use PWWEB\Artomator\Commands\Common\RepositoryGeneratorCommand;
-use PWWEB\Artomator\Commands\Publish\GeneratorPublishCommand;
-use PWWEB\Artomator\Commands\Publish\LayoutPublishCommand;
-use PWWEB\Artomator\Commands\Publish\PublishTemplateCommand;
-use PWWEB\Artomator\Commands\Publish\VueJsLayoutPublishCommand;
-use PWWEB\Artomator\Commands\RollbackGeneratorCommand;
-use PWWEB\Artomator\Commands\Scaffold\ControllerGeneratorCommand;
-use PWWEB\Artomator\Commands\Scaffold\RequestsGeneratorCommand;
-use PWWEB\Artomator\Commands\Scaffold\ScaffoldGeneratorCommand;
-use PWWEB\Artomator\Commands\Scaffold\ViewsGeneratorCommand;
-use PWWEB\Artomator\Commands\VueJs\VueJsGeneratorCommand;
+use InfyOm\Generator\InfyOmGeneratorServiceProvider as ServiceProvider;
+use PWWEB\Artomator\Commands\GraphQL\GraphQLGeneratorCommand;
+use PWWEB\Artomator\Commands\GraphQL\GraphQLQueryGeneratorCommand;
+use PWWEB\Artomator\Commands\GraphQL\GraphQLMutationsGeneratorCommand;
+use PWWEB\Artomator\Commands\GraphQL\GraphQLTypeGeneratorCommand;
+use PWWEB\Artomator\Commands\GraphQLScaffoldGeneratorCommand;
 
 class ArtomatorServiceProvider extends ServiceProvider
 {
@@ -39,6 +25,7 @@ class ArtomatorServiceProvider extends ServiceProvider
             $configPath => config_path('pwweb/artomator.php'),
             ]
         );
+        parent::boot();
     }
 
     /**
@@ -48,140 +35,46 @@ class ArtomatorServiceProvider extends ServiceProvider
      */
     public function register()
     {
+
         $this->app->singleton(
-            'artomator.publish', function ($app) {
-                return new GeneratorPublishCommand();
+            'artomator.graphql', function ($app) {
+                return new GraphQLGeneratorCommand();
             }
         );
 
         $this->app->singleton(
-            'artomator.api', function ($app) {
-                return new APIGeneratorCommand();
+            'artomator.graphql_scaffold', function ($app) {
+                return new GraphQLScaffoldGeneratorCommand();
             }
         );
 
         $this->app->singleton(
-            'artomator.scaffold', function ($app) {
-                return new ScaffoldGeneratorCommand();
+            'artomator.graphql.query', function ($app) {
+                return new GraphQLQueryGeneratorCommand();
             }
         );
 
         $this->app->singleton(
-            'artomator.publish.layout', function ($app) {
-                return new LayoutPublishCommand();
+            'artomator.graphql.mutations', function ($app) {
+                return new GraphQLMutationsGeneratorCommand();
             }
         );
 
         $this->app->singleton(
-            'artomator.publish.templates', function ($app) {
-                return new PublishTemplateCommand();
+            'artomator.graphql.type', function ($app) {
+                return new GraphQLTypeGeneratorCommand();
             }
         );
 
-        $this->app->singleton(
-            'artomator.api_scaffold', function ($app) {
-                return new APIScaffoldGeneratorCommand();
-            }
-        );
-
-        $this->app->singleton(
-            'artomator.migration', function ($app) {
-                return new MigrationGeneratorCommand();
-            }
-        );
-
-        $this->app->singleton(
-            'artomator.model', function ($app) {
-                return new ModelGeneratorCommand();
-            }
-        );
-
-        $this->app->singleton(
-            'artomator.repository', function ($app) {
-                return new RepositoryGeneratorCommand();
-            }
-        );
-
-        $this->app->singleton(
-            'artomator.api.query', function ($app) {
-                return new APIQueryGeneratorCommand();
-            }
-        );
-
-        $this->app->singleton(
-            'artomator.api.mutations', function ($app) {
-                return new APIMutationsGeneratorCommand();
-            }
-        );
-
-        $this->app->singleton(
-            'artomator.api.type', function ($app) {
-                return new APITypeGeneratorCommand();
-            }
-        );
-
-        $this->app->singleton(
-            'artomator.api.config', function ($app) {
-                return new APIConfigGeneratorCommand();
-            }
-        );
-
-        $this->app->singleton(
-            'artomator.scaffold.controller', function ($app) {
-                return new ControllerGeneratorCommand();
-            }
-        );
-
-        $this->app->singleton(
-            'artomator.scaffold.requests', function ($app) {
-                return new RequestsGeneratorCommand();
-            }
-        );
-
-        $this->app->singleton(
-            'artomator.scaffold.views', function ($app) {
-                return new ViewsGeneratorCommand();
-            }
-        );
-
-        $this->app->singleton(
-            'artomator.rollback', function ($app) {
-                return new RollbackGeneratorCommand();
-            }
-        );
-
-        $this->app->singleton(
-            'artomator.vuejs', function ($app) {
-                return new VueJsGeneratorCommand();
-            }
-        );
-        $this->app->singleton(
-            'artomator.publish.vuejs', function ($app) {
-                return new VueJsLayoutPublishCommand();
-            }
-        );
+        parent::register();
 
         $this->commands(
             [
-            'artomator.publish',
-            'artomator.api',
-            'artomator.scaffold',
-            'artomator.api_scaffold',
-            'artomator.publish.layout',
-            'artomator.publish.templates',
-            'artomator.migration',
-            'artomator.model',
-            'artomator.repository',
-            'artomator.api.query',
-            'artomator.api.mutations',
-            'artomator.api.type',
-            'artomator.api.config',
-            'artomator.scaffold.controller',
-            'artomator.scaffold.requests',
-            'artomator.scaffold.views',
-            'artomator.rollback',
-            'artomator.vuejs',
-            'artomator.publish.vuejs',
+            'artomator.graphql',
+            'artomator.graphql_scaffold',
+            'artomator.graphql.query',
+            'artomator.graphql.mutations',
+            'artomator.graphql.type',
             ]
         );
     }
--- src/helpers.php
+++ src/helpers.php
@@ -1,82 +1,6 @@
 <?php
 
-use Illuminate\Support\Str;
-use PWWEB\Artomator\Common\GeneratorField;
-
-if (!function_exists('arty_tab')) {
-    /**
-     * Generates tab with spaces.
-     *
-     * @param int $spaces
-     *
-     * @return string
-     */
-    function arty_tab($spaces = 4)
-    {
-        return str_repeat(' ', $spaces);
-    }
-}
-
-if (!function_exists('arty_tabs')) {
-    /**
-     * Generates tab with spaces.
-     *
-     * @param int $tabs
-     * @param int $spaces
-     *
-     * @return string
-     */
-    function arty_tabs($tabs, $spaces = 4)
-    {
-        return str_repeat(arty_tab($spaces), $tabs);
-    }
-}
-
-if (!function_exists('arty_nl')) {
-    /**
-     * Generates new line char.
-     *
-     * @param int $count
-     *
-     * @return string
-     */
-    function arty_nl($count = 1)
-    {
-        return str_repeat(PHP_EOL, $count);
-    }
-}
-
-if (!function_exists('arty_nls')) {
-    /**
-     * Generates new line char.
-     *
-     * @param int $count
-     * @param int $nls
-     *
-     * @return string
-     */
-    function arty_nls($count, $nls = 1)
-    {
-        return str_repeat(arty_nl($nls), $count);
-    }
-}
-
-if (!function_exists('arty_nl_tab')) {
-    /**
-     * Generates new line char.
-     *
-     * @param int $lns
-     * @param int $tabs
-     *
-     * @return string
-     */
-    function arty_nl_tab($lns = 1, $tabs = 1)
-    {
-        return arty_nls($lns).arty_tabs($tabs);
-    }
-}
-
-if (!function_exists('get_template_file_path')) {
+if (! function_exists('get_template_file_path')) {
     /**
      * get path for template file.
      *
@@ -87,6 +11,8 @@ if (!function_exists('get_template_file_path')) {
      */
     function get_template_file_path($templateName, $templateType)
     {
+        echo "PWWEB FILE\n";
+        die($templateName);
         $templateName = str_replace('.', '/', $templateName);
 
         $templatesPath = config(
@@ -105,94 +31,5 @@ if (!function_exists('get_template_file_path')) {
         }
 
         return base_path('vendor/infyomlabs/'.$templateType.'/templates/'.$templateName.'.stub');
-    }
-}
-
-if (!function_exists('get_template')) {
-    /**
-     * get template contents.
-     *
-     * @param string $templateName
-     * @param string $templateType
-     *
-     * @return string
-     */
-    function get_template($templateName, $templateType)
-    {
-        $path = get_template_file_path($templateName, $templateType);
-
-        return file_get_contents($path);
-    }
-}
-
-if (!function_exists('fill_template')) {
-    /**
-     * fill template with variable values.
-     *
-     * @param array  $variables
-     * @param string $template
-     *
-     * @return string
-     */
-    function fill_template($variables, $template)
-    {
-        foreach ($variables as $variable => $value) {
-            $template = str_replace($variable, $value, $template);
-        }
-
-        return $template;
-    }
-}
-
-if (!function_exists('fill_field_template')) {
-    /**
-     * fill field template with variable values.
-     *
-     * @param array          $variables
-     * @param string         $template
-     * @param GeneratorField $field
-     *
-     * @return string
-     */
-    function fill_field_template($variables, $template, $field)
-    {
-        foreach ($variables as $variable => $key) {
-            $template = str_replace($variable, $field->$key, $template);
-        }
-
-        return $template;
-    }
-}
-
-if (!function_exists('fill_template_with_field_data')) {
-    /**
-     * fill template with field data.
-     *
-     * @param array          $variables
-     * @param array          $fieldVariables
-     * @param string         $template
-     * @param GeneratorField $field
-     *
-     * @return string
-     */
-    function fill_template_with_field_data($variables, $fieldVariables, $template, $field)
-    {
-        $template = fill_template($variables, $template);
-
-        return fill_field_template($fieldVariables, $template, $field);
-    }
-}
-
-if (!function_exists('model_name_from_table_name')) {
-    /**
-     * generates model name from table name.
-     *
-     * @param string $tableName
-     *
-     * @return string
-     */
-    function model_name_from_table_name($tableName)
-    {
-        return Str::ucfirst(Str::camel(Str::singular($tableName)));
     }
 }
