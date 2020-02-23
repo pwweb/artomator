983d6b465a65c9511bf72c2120f6b6e1e05f560a
Richard Browne <richard@my-sauce.co.uk>
2020-02-23 16:57:43 +0000

    Trim down of config file to the essential extras

--- config/artomator.php
+++ config/artomator.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Paths
@@ -10,49 +9,6 @@ return [
     */
 
     'path' => [
-
-        'migration'         => database_path('migrations/'),
-
-        'model'             => app_path('Models/'),
-
-        'datatables'        => app_path('DataTables/'),
-
-        'repository'        => app_path('Repositories/'),
-
-        'routes'            => base_path('routes/web.php'),
-
-        'api_routes'        => base_path('routes/api.php'),
-
-        'request'           => app_path('Http/Requests/'),
-
-        'api_mutation'       => app_path('Http/GraphQL/Mutations/'),
-
-        'controller'        => app_path('Http/Controllers/'),
-
-        'api_query'         => app_path('Http/GraphQL/Queries/'),
-
-        'api_type'         => app_path('Http/GraphQL/Types/'),
-
-        'repository_test'   => base_path('tests/Repositories/'),
-
-        'api_test'          => base_path('tests/APIs/'),
-
-        'tests'             => base_path('tests/'),
-
-        'views'             => resource_path('views/'),
-
-        'schema_files'      => resource_path('model_schemas/'),
-
-        'templates_dir'     => resource_path('pwweb/artomator-templates/'),
-
-        'seeder'            => database_path('seeds/'),
-
-        'database_seeder'   => database_path('seeds/DatabaseSeeder.php'),
-
-        'modelJs'           => resource_path('assets/js/models/'),
-
-        'factory'           => database_path('factories/'),
-
         'query'             => app_path('GraphQL/Query/'),
 
         'type'              => app_path('GraphQL/Type/'),
@@ -66,155 +22,9 @@ return [
     */
 
     'namespace' => [
+        'query'             => 'App\GraphQL\Query',
 
-        'model'             => 'App\Models',
-
-        'datatables'        => 'App\DataTables',
-
-        'repository'        => 'App\Repositories',
-
-        'controller'        => 'App\Http\Controllers',
-
-        'api_query'         => 'App\Http\GraphQL\Queries',
-
-        'request'           => 'App\Http\Requests',
-
-        'api_mutation'      => 'App\Http\GraphQL\Mutations',
-
-        'repository_test'   => 'Tests\Repositories',
-
-        'api_type'          => 'App\Http\GraphQL\Types',
-
-        'tests'             => 'Tests',
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Templates
-    |--------------------------------------------------------------------------
-    |
-    */
-
-    'templates'         => 'coreui-templates',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Model extend class
-    |--------------------------------------------------------------------------
-    |
-    */
-
-    'model_extend_class' => 'Eloquent',
-
-    /*
-    |--------------------------------------------------------------------------
-    | API routes prefix & version
-    |--------------------------------------------------------------------------
-    |
-    */
-
-    'api_prefix'  => 'api',
-
-    'api_version' => 'v1',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Options
-    |--------------------------------------------------------------------------
-    |
-    */
-
-    'options' => [
-
-        'softDelete' => true,
-
-        'seeders' => false,
-
-        'save_schema_file' => true,
-
-        'tables_searchable_default' => false,
-
-        'repository_pattern' => true,
-
-        'excluded_fields' => ['id'], // Array of columns that doesn't required while creating module
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Prefixes
-    |--------------------------------------------------------------------------
-    |
-    */
-
-    'prefixes' => [
-
-        'route' => '',  // using admin will create route('admin.?.index') type routes
-
-        'path' => '',
-
-        'view' => '',  // using backend will create return view('backend.?.index') type the backend views directory
-
-        'public' => '',
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Add-Ons
-    |--------------------------------------------------------------------------
-    |
-    */
-
-    'add_on' => [
-
-        'swagger'       => false,
-
-        'tests'         => true,
-
-        'datatables'    => false,
-
-        'menu'          => [
-
-            'enabled'       => true,
-
-            'menu_file'     => 'layouts/menu.blade.php',
-        ],
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Timestamp Fields
-    |--------------------------------------------------------------------------
-    |
-    */
-
-    'timestamps' => [
-
-        'enabled'       => true,
-
-        'created_at'    => 'created_at',
-
-        'updated_at'    => 'updated_at',
-
-        'deleted_at'    => 'deleted_at',
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Save model files to `App/Models` when use `--prefix`. see #208
-    |--------------------------------------------------------------------------
-    |
-    */
-    'ignore_model_prefix' => false,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Specify custom doctrine mappings as per your need
-    |--------------------------------------------------------------------------
-    |
-    */
-    'from_table' => [
-
-        'doctrine_mappings' => [],
+        'type'              => 'App\GraphQL\Type',
     ],
 
     /*
@@ -224,7 +34,7 @@ return [
     |
     */
     'license' => [
-
+        'package' => 'Incredible Package Name',
         'authors' => [
             'Jane Doe <jane.doe@example.com>',
             'Joe Blogs <joe.blogs@example.com>',
@@ -232,7 +42,6 @@ return [
 
         'copyright' => '2019 ACME Corp.',
 
-        'file' => 'license.md',
+        'license' => 'license.md All rights reserved.',
     ],
-
 ];
