8f3d7e43c9ac2e372ba6a7edb3cd9f70b93bebfc
Richard Browne <richard@my-sauce.co.uk>
2020-03-31 18:50:22 +0100

    Full replication of the base command

--- src/Commands/Publish/GeneratorPublishCommand.php
+++ src/Commands/Publish/GeneratorPublishCommand.php
@@ -8,6 +8,11 @@ use Symfony\Component\Console\Input\InputOption;
 class GeneratorPublishCommand extends PublishBaseCommand
 {
     /**
+     * ALL REFERENCES TO (get_template\(\')([a-z_\.]+)(', 'laravel-generator'\))
+     * REPLACED WITH: get_artomator_template('$2')
+     */
+
+    /**
      * The console command name.
      *
      * @var string
@@ -41,12 +46,35 @@ class GeneratorPublishCommand extends PublishBaseCommand
 
     /**
      * Replaces dynamic variables of template.
+     * THIS IS A NEW FUNCTION ADDED.
      *
      * @param string $templateData
      *
      * @return string
      */
-    private function fillTemplate($templateData)
+    private function fillLicense($templateData)
+    {
+        $replacements = [
+            '$LICENSE_PACKAGE$' => config('pwweb.artomator.license.package', 'boo'),
+            '$LICENSE_AUTHORS$' => license_authors(config('pwweb.artomator.license.authors')),
+            '$LICENSE_COPYRIGHT$' => config('pwweb.artomator.license.copyright'),
+            '$LICENSE$' => config('pwweb.artomator.license.license'),
+        ];
+        foreach ($replacements as $key => $replacement) {
+            $templateData = str_replace($key, $replacement, $templateData);
+        }
+
+        return $templateData;
+    }
+
+    /**
+     * Replaces dynamic variables of template.
+     *
+     * @param string $templateData
+     *
+     * @return string
+     */
+    protected function fillTemplate($templateData)
     {
         $apiVersion = config('infyom.laravel_generator.api_version', 'v1');
         $apiPrefix = config('infyom.laravel_generator.api_prefix', 'api');
@@ -57,7 +85,9 @@ class GeneratorPublishCommand extends PublishBaseCommand
         $appNamespace = substr($appNamespace, 0, strlen($appNamespace) - 1);
         $templateData = str_replace('$NAMESPACE_APP$', $appNamespace, $templateData);
 
-        return $templateData;
+        // return $templateData;
+        // ADDED THE FOLLOWING LINE:
+        return $this->fillLicense($templateData);
     }
 
     private function publishTestCases()
@@ -67,10 +97,13 @@ class GeneratorPublishCommand extends PublishBaseCommand
         $createdAtField = config('infyom.laravel_generator.timestamps.created_at', 'created_at');
         $updatedAtField = config('infyom.laravel_generator.timestamps.updated_at', 'updated_at');
 
-        $templateData = get_template('test.api_test_trait', 'laravel-generator');
+        $templateData = get_artomator_template('test.api_test_trait');
 
         $templateData = str_replace('$NAMESPACE_TESTS$', $testsNameSpace, $templateData);
         $templateData = str_replace('$TIMESTAMPS$', "['$createdAtField', '$updatedAtField']", $templateData);
+
+        // ADDED THE FOLLOWING LINE:
+        $templateData = $this->fillLicense($templateData);
 
         $fileName = 'ApiTestTrait.php';
 
@@ -96,7 +129,8 @@ class GeneratorPublishCommand extends PublishBaseCommand
 
     private function publishBaseController()
     {
-        $templateData = get_template('app_base_controller', 'laravel-generator');
+        $this->info('Boo!');
+        $templateData = get_artomator_template('app_base_controller');
 
         $templateData = $this->fillTemplate($templateData);
 
@@ -115,7 +149,7 @@ class GeneratorPublishCommand extends PublishBaseCommand
 
     private function publishBaseRepository()
     {
-        $templateData = get_template('base_repository', 'laravel-generator');
+        $templateData = get_artomator_template('base_repository');
 
         $templateData = $this->fillTemplate($templateData);
 
