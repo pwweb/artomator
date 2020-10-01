5b9a8903288be869aaf26753d5fcd8e83a010b47
Richard Browne <rabrowne85@users.noreply.github.com>
2020-10-01 17:33:01 +0100

    Merging issues (#47)
    * Fix issue #42 - custom routes
    
    * Apply fixes from StyleCI (#43)
    
    * Updated templates for Issue #44
    
    * Switch order for code - Issue #42
    
    * Ability to regenerate routes - Issue #42
    
    * Apply fixes from StyleCI (#46)
    
    * Upgrading to Laravel/Framework 8
    
    * Reverses infyomlabs upgrade
    
    Co-authored-by: Frank Pillukeit <clients@pw-websolutions.com>

--- src/ArtomatorServiceProvider.php
+++ src/ArtomatorServiceProvider.php
@@ -24,6 +24,7 @@ use PWWEB\Artomator\Commands\Publish\PublishUserCommand;
 use PWWEB\Artomator\Commands\RollbackGeneratorCommand;
 use PWWEB\Artomator\Commands\Scaffold\ControllerGeneratorCommand;
 use PWWEB\Artomator\Commands\Scaffold\RequestsGeneratorCommand;
+use PWWEB\Artomator\Commands\Scaffold\RoutesGeneratorCommand;
 use PWWEB\Artomator\Commands\Scaffold\ScaffoldGeneratorCommand;
 use PWWEB\Artomator\Commands\Scaffold\ViewsGeneratorCommand;
 
@@ -56,100 +57,178 @@ class ArtomatorServiceProvider extends ServiceProvider
 
     /**
      * Register the application services.
+     *
+     * @return void
      */
     public function register()
     {
-        $this->app->singleton('artomator.publish', function ($app) {
-            return new GeneratorPublishCommand();
-        });
+        $this->app->singleton(
+            'artomator.publish',
+            function ($app) {
+                return new GeneratorPublishCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.api', function ($app) {
-            return new APIGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.api',
+            function ($app) {
+                return new APIGeneratorCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.scaffold', function ($app) {
-            return new ScaffoldGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.scaffold',
+            function ($app) {
+                return new ScaffoldGeneratorCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.publish.layout', function ($app) {
-            return new LayoutPublishCommand();
-        });
+        $this->app->singleton(
+            'artomator.publish.layout',
+            function ($app) {
+                return new LayoutPublishCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.publish.templates', function ($app) {
-            return new PublishTemplateCommand();
-        });
+        $this->app->singleton(
+            'artomator.publish.templates',
+            function ($app) {
+                return new PublishTemplateCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.api_scaffold', function ($app) {
-            return new APIScaffoldGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.api_scaffold',
+            function ($app) {
+                return new APIScaffoldGeneratorCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.migration', function ($app) {
-            return new MigrationGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.migration',
+            function ($app) {
+                return new MigrationGeneratorCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.model', function ($app) {
-            return new ModelGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.model',
+            function ($app) {
+                return new ModelGeneratorCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.repository', function ($app) {
-            return new RepositoryGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.repository',
+            function ($app) {
+                return new RepositoryGeneratorCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.api.controller', function ($app) {
-            return new APIControllerGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.api.controller',
+            function ($app) {
+                return new APIControllerGeneratorCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.api.requests', function ($app) {
-            return new APIRequestsGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.api.requests',
+            function ($app) {
+                return new APIRequestsGeneratorCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.api.tests', function ($app) {
-            return new TestsGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.api.tests',
+            function ($app) {
+                return new TestsGeneratorCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.scaffold.controller', function ($app) {
-            return new ControllerGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.scaffold.controller',
+            function ($app) {
+                return new ControllerGeneratorCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.scaffold.requests', function ($app) {
-            return new RequestsGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.scaffold.requests',
+            function ($app) {
+                return new RequestsGeneratorCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.scaffold.views', function ($app) {
-            return new ViewsGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.scaffold.views',
+            function ($app) {
+                return new ViewsGeneratorCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.rollback', function ($app) {
-            return new RollbackGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.scaffold.routes',
+            function ($app) {
+                return new RoutesGeneratorCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.publish.user', function ($app) {
-            return new PublishUserCommand();
-        });
+        $this->app->singleton(
+            'artomator.rollback',
+            function ($app) {
+                return new RollbackGeneratorCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.graphql', function ($app) {
-            return new GraphQLGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.publish.user',
+            function ($app) {
+                return new PublishUserCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.graphql_scaffold', function ($app) {
-            return new GraphQLScaffoldGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.graphql',
+            function ($app) {
+                return new GraphQLGeneratorCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.graphql.query', function ($app) {
-            return new GraphQLQueryGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.graphql_scaffold',
+            function ($app) {
+                return new GraphQLScaffoldGeneratorCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.graphql.mutations', function ($app) {
-            return new GraphQLMutationsGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.graphql.query',
+            function ($app) {
+                return new GraphQLQueryGeneratorCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.graphql.type', function ($app) {
-            return new GraphQLTypeGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.graphql.mutations',
+            function ($app) {
+                return new GraphQLMutationsGeneratorCommand();
+            }
+        );
 
-        $this->app->singleton('artomator.graphql.subscription', function ($app) {
-            return new GraphQLSubscriptionGeneratorCommand();
-        });
+        $this->app->singleton(
+            'artomator.graphql.type',
+            function ($app) {
+                return new GraphQLTypeGeneratorCommand();
+            }
+        );
+
+        $this->app->singleton(
+            'artomator.graphql.subscription',
+            function ($app) {
+                return new GraphQLSubscriptionGeneratorCommand();
+            }
+        );
 
         parent::register();
 
@@ -170,6 +249,7 @@ class ArtomatorServiceProvider extends ServiceProvider
                 'artomator.scaffold.controller',
                 'artomator.scaffold.requests',
                 'artomator.scaffold.views',
+                'artomator.scaffold.routes',
                 'artomator.rollback',
                 'artomator.publish.user',
                 'artomator.graphql',
--- src/Generators/Scaffold/RoutesGenerator.php
+++ src/Generators/Scaffold/RoutesGenerator.php
@@ -8,48 +8,61 @@ use PWWEB\Artomator\Common\CommandData;
 class RoutesGenerator
 {
     /**
+     * Command data instance.
+     *
      * @var CommandData
      */
     private $commandData;
 
     /**
+     * Path variable.
+     *
      * @var string
      */
     private $path;
 
     /**
+     * Route Contents.
+     *
      * @var string
      */
     private $routeContents;
 
     /**
+     * Route template.
+     *
      * @var string
      */
     private $routesTemplate;
 
     /**
+     * Routes array.
+     *
      * @var string
      */
     private $routes;
 
+    /**
+     * Constructor.
+     *
+     * @param CommandData $commandData Command data passed in from above.
+     */
     public function __construct(CommandData $commandData)
     {
         $this->commandData = $commandData;
         $this->path = $commandData->config->pathRoutes;
-        $this->prepareRoutes();
-        $this->routeContents = file_get_contents($this->path);
-        if (1 !== preg_match('/\/\/ Artomator Routes Start(.*)\/\/ Artomator Routes Stop/sU', $this->routeContents)) {
-            $this->routeContents .= "\n\n// Artomator Routes Start\n// Artomator Routes Stop";
-        }
-
-        $this->routeContents = preg_replace('/(\/\/ Artomator Routes Start)(.*)(\/\/ Artomator Routes Stop)/sU', "$1\n".$this->routes.'$3', $this->routeContents);
     }
 
+    /**
+     * Prepare the routes array.
+     *
+     * @return void
+     */
     public function prepareRoutes()
     {
         $fileName = $this->path.'.json';
 
-        if (file_exists($fileName)) {
+        if (file_exists($fileName) === true) {
             // Routes json exists:
             $fileRoutes = file_get_contents($fileName);
             $fileRoutes = json_decode($fileRoutes, true);
@@ -57,7 +70,7 @@ class RoutesGenerator
             $fileRoutes = [];
         }
 
-        if (empty($this->commandData->config->prefixes['route'])) {
+        if (empty($this->commandData->config->prefixes['route']) === true) {
             $new = [
                 'resources' => [$this->commandData->modelName => $this->commandData->modelName],
                 'name'      => strtolower($this->commandData->modelName),
@@ -78,7 +91,7 @@ class RoutesGenerator
                 }
                 $routes = [ucfirst($prefix) => $new];
             }
-        }//end if
+        }
         $fileRoutes = array_replace_recursive($fileRoutes, $routes);
         file_put_contents($fileName, json_encode($fileRoutes, JSON_PRETTY_PRINT));
         $this->commandData->commandComment("\nRoute JSON File saved: ");
@@ -86,26 +99,98 @@ class RoutesGenerator
         $this->routes = $this->buildText($fileRoutes);
     }
 
+    /**
+     * Generator function.
+     *
+     * @return void
+     */
     public function generate()
     {
+        $this->prepareRoutes();
+        $this->routeContents = file_get_contents($this->path);
+        if (1 !== preg_match('/\/\/ Artomator Routes Start(.*)\/\/ Artomator Routes Stop/sU', $this->routeContents)) {
+            $this->routeContents .= "\n\n// Artomator Routes Start\n// Artomator Routes Stop";
+        }
+
+        $this->routeContents = preg_replace('/(\/\/ Artomator Routes Start)(.*)(\/\/ Artomator Routes Stop)/sU', "$1\n".$this->routes.'$3', $this->routeContents);
+
         file_put_contents($this->path, $this->routeContents);
         $this->commandData->commandComment("\n".$this->commandData->config->mCamelPlural.' routes added.');
     }
 
+    /**
+     * Re-generator the routes function.
+     *
+     * @return void
+     */
+    public function regenerate()
+    {
+        $fileName = $this->path.'.json';
+
+        if (file_exists($fileName) === true) {
+            // Routes json exists:
+            $fileRoutes = file_get_contents($fileName);
+            $fileRoutes = json_decode($fileRoutes, true);
+        } else {
+            $fileRoutes = [];
+        }
+        $this->routes = $this->buildText($fileRoutes);
+        $this->routeContents = file_get_contents($this->path);
+        if (1 !== preg_match('/\/\/ Artomator Routes Start(.*)\/\/ Artomator Routes Stop/sU', $this->routeContents)) {
+            $this->routeContents .= "\n\n// Artomator Routes Start\n// Artomator Routes Stop";
+        }
+
+        $this->routeContents = preg_replace('/(\/\/ Artomator Routes Start)(.*)(\/\/ Artomator Routes Stop)/sU', "$1\n".$this->routes.'$3', $this->routeContents);
+
+        file_put_contents($this->path, $this->routeContents);
+        $this->commandData->commandComment("\nRoutes regenerated.");
+    }
+
+    /**
+     * Rollback function.
+     *
+     * @return void
+     */
     public function rollback()
     {
-        if (Str::contains($this->routeContents, $this->routesTemplate)) {
+        if (Str::contains($this->routeContents, $this->routesTemplate) === true) {
             $this->routeContents = str_replace($this->routesTemplate, '', $this->routeContents);
             file_put_contents($this->path, $this->routeContents);
             $this->commandData->commandComment('scaffold routes deleted');
         }
     }
 
-    private function buildText($routes, $indent = 0)
+    /**
+     * Template text builder function.
+     *
+     * @param array $routes Routes array to process
+     * @param int   $indent Indent counter
+     *
+     * @return void
+     */
+    private function buildText(array $routes, int $indent = 0)
     {
         $templateContent = '';
         foreach ($routes as $route_key => $route) {
             $templateString = '';
+            $tabs = (isset($route['prefix']) === true) ? (($indent * 3) + 3) : 0;
+            if (isset($route['custom']) === true) {
+                foreach ($route['custom'] as $custom_key => $custom) {
+                    if (true === isset($custom['function']) && $custom['function'] !== '') {
+                        $custom['function'] = '@'.$custom['function'];
+                    }
+                    $vars = [
+                        '$ITERATION_CUSTOM_METHOD$' => $custom['method'],
+                        '$ITERATION_CUSTOM_ENDPOINT$' => $custom['endpoint'],
+                        '$ITERATION_CUSTOM_CONTROLLER$' => $custom['controller'],
+                        '$ITERATION_CUSTOM_FUNCTION$' => $custom['function'],
+                        '$ITERATION_CUSTOM_NAME$' => $custom['name'],
+                        '$INDENT$' => infy_tabs($tabs),
+                    ];
+                    $templateString .= get_artomator_template('scaffold.routes.prefixed.custom');
+                    $templateString = fill_template($vars, $templateString);
+                }
+            }
             if (isset($route['resources'])) {
                 $tabs = (isset($route['prefix'])) ? (($indent * 3) + 3) : 0;
                 foreach (array_keys($route['resources']) as $resource_key) {
@@ -118,10 +203,10 @@ class RoutesGenerator
                     $templateString = fill_template($vars, $templateString);
                 }
             }
-            if ((isset($route['group']))) {
+            if ((isset($route['group'])) === true) {
                 $templateString .= $this->buildText($route['group'], ($indent + 1));
             }
-            if ((isset($route['prefix']))) {
+            if ((isset($route['prefix'])) === true) {
                 $vars = [
                     '$ITERATION_NAMESPACE_CAMEL$' => ucfirst($route_key),
                     '$ITERATION_NAMESPACE_LOWER$' => strtolower($route_key),
@@ -131,7 +216,7 @@ class RoutesGenerator
                 $templateString = fill_template($vars, $templateString);
             }
             $templateContent .= $templateString;
-        }//end foreach
+        }
 
         return $templateContent;
     }
