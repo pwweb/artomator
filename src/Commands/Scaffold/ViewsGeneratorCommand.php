505e6c770b5da8510c2c5e36ac1a60e1480477ea
Richard Browne <r.a.browne@gmail.com>
2021-04-07 14:32:35 +0100

    🔧 Allow the generation of the Vue Views from the scaffold.

--- src/Commands/Scaffold/ViewsGeneratorCommand.php
+++ src/Commands/Scaffold/ViewsGeneratorCommand.php
@@ -4,6 +4,9 @@ namespace PWWEB\Artomator\Commands\Scaffold;
 
 use InfyOm\Generator\Commands\Scaffold\ViewsGeneratorCommand as Base;
 use PWWEB\Artomator\Common\CommandData;
+use PWWEB\Artomator\Generators\Scaffold\ViewGenerator;
+use PWWEB\Artomator\Generators\Scaffold\VueGenerator;
+use Symfony\Component\Console\Input\InputOption;
 
 class ViewsGeneratorCommand extends Base
 {
@@ -22,5 +25,43 @@ class ViewsGeneratorCommand extends Base
         parent::__construct();
 
         $this->commandData = new CommandData($this, CommandData::$COMMAND_TYPE_SCAFFOLD);
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->commandData->modelName = $this->argument('model');
+
+        $this->commandData->initCommandData();
+        $this->commandData->getFields();
+
+        if (false === $this->commandData->getOption('vue')) {
+            $viewGenerator = new ViewGenerator($this->commandData);
+            $viewGenerator->generate();
+        } else {
+            $vueGenerator = new VueGenerator($this->commandData);
+            $vueGenerator->generate();
+        }
+
+        $this->performPostActions();
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    public function getOptions()
+    {
+        return array_merge(
+            parent::getOptions(),
+            [
+                ['vue', null, InputOption::VALUE_NONE, 'Generate Vuejs views rather than blade views'],
+            ]
+        );
     }
 }
